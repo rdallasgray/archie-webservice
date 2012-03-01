79b9d5caf94da5559cabc7965b7f6390fb3d02a9
Robert Dallas Gray <mail@robertdallasgray.com>
2012-03-01 10:48:32 +0000

    Switched db to postgres

--- archie/config.py
+++ archie/config.py
@@ -1,11 +1,11 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///archie.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://archie:Antonine2008@localhost/archie'
     WORLD_PROVIDER_ID = 'archie-antonine'
     WORLD_PROVIDER_URL = 'http://archie-antonine.org'
     WORLD_NAME = 'ARCHIE: A Walking Tour on the Antonine Wall'
-    WORLD_DESCRIPTION = 'Walking tours on the Antonine Wall'
+    WORLD_DESCRIPTION = 'A Walking Tour on the Antonine Wall'
     WORLD_TAGS = 'walking, scotland, antonine, roman, history'
     WORLD_LOGO_URL = '/assets/world_logo.png'
     WORLD_ICON_URL = '/assets/world_icon.png'
