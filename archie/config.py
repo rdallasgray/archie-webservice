2464c1135c24a61d7a11d68feeb1308e99df81b8
Robert Dallas Gray <mail@robertdallasgray.com>
2012-03-08 08:56:27 +0000

    Ignored shell files, amended provider url, removed world icon and logo references

--- archie/config.py
+++ archie/config.py
@@ -6,12 +6,12 @@ class Config(object):
     LOCAL_DATABASE_URL = 'postgres://archie:Antonine2008@localhost/archie'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', LOCAL_DATABASE_URL)
     WORLD_PROVIDER_ID = 'archie-antonine'
-    WORLD_PROVIDER_URL = 'http://archie-antonine.org'
+    WORLD_PROVIDER_URL = 'http://vivid-mist-1942.herokuapp.com/pois'
     WORLD_NAME = 'ARCHIE: A Walking Tour on the Antonine Wall'
     WORLD_DESCRIPTION = 'A Walking Tour on the Antonine Wall'
     WORLD_TAGS = 'walking, scotland, antonine, roman, history'
-    WORLD_LOGO_URL = '/assets/world_logo.png'
-    WORLD_ICON_URL = '/assets/world_icon.png'
+    #    WORLD_LOGO_URL = 
+    #    WORLD_ICON_URL = 
     WORLD_SHORT_NAME = 'ARCHIE'
 
 class DevelopmentConfig(Config):
