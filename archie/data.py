ed49715ef9448a4942b29ff8ab53a55f44d229a4
Robert Dallas Gray <mail@robertdallasgray.com>
2012-03-08 09:58:50 +0000

    Testing with minimal data

--- archie/data.py
+++ archie/data.py
@@ -3,175 +3,175 @@ data = [
         "name": "Kelvin Stevenson Memorial Parish Church",
         "description": "1. Kelvin Stevenson Memorial Church. Follow the steps down to Garriochmill Road.",
         "co_ordinates": "55.877773,-4.278837,33"
-    },
-    {
-        "name": "North Woodside Flint Mill",
-        "description": "2. Remains of North Woodside Flint Mill.",
-        "co_ordinates": "55.877963,-4.282098,33"
-    },
-    {
-        "name": "Queen Margaret Bridge",
-        "description": "3. Queen Margaret Bridge.",
-        "co_ordinates": "55.879589,-4.285663,32"
-    },
-    {
-        "name": "Bridge to Botanic Gardens",
-        "description": "4. Bridge to Botanic Gardens.",
-        "co_ordinates": "55.880067,-4.287890,32"
-    },
-    {
-        "name": "Ha'penny Bridge House",
-        "description": "5. Ha'penny Bridge House. Take right fork following the River Kelvin on its South bank.",
-        "co_ordinates": "55.881264,-4.291263,29"
-    },
-    {
-        "name": "Return bridge from Botanic Gardens Arboretum",
-        "description": "6. Return bridge from Botanic Gardens Arboretum. Continue North on Kelvin Walkway.",
-        "co_ordinates": "55.884409,-4.289731,32"
-    },
-    {
-        "name": "Disused railway bridges",
-        "description": "7. Disused railway bridges.",
-        "co_ordinates": "55.885111,-4.291141,31"
-    },
-    {
-        "name": "Fork to Wyndford Estate",
-        "description": "8. Fork to Wyndford Estate. Follow Kelvin Walkway West.",
-        "co_ordinates": "55.885402,-4.292085,31"
-    },
-    {
-        "name": "Kelvindale Road",
-        "description": "9. Kelvindale Road. Cross and continue North on Kelvin Walkway.",
-        "co_ordinates": "55.887585,-4.298239,28"
-    },
-    {
-        "name": "Kelvin Aqueduct, turn-off for Forth and Clyde Canal",
-        "description": "10. Kelvin Aqueduct, turn-off for Forth and Clyde Canal. Continue North on Kelvin Walkway.",
-        "co_ordinates": "55.892040,-4.300411,36"
-    },
-    {
-        "name": "Maryhill Rail Bridge",
-        "description": "11. Maryhill Rail Bridge. Follow Kelvin Walkway Northwest.",
-        "co_ordinates": "55.896456,-4.303370,34"
-    },
-    {
-        "name": "Footbridge to Dawsholm Park",
-        "description": "12. Footbridge to Dawsholm Park. Turn right shortly afterward, heading uphill away from the river.",
-        "co_ordinates": "55.899041,-4.306004,39"
-    },
-    {
-        "name": "Entrance to A81 footpath",
-        "description": "13. Entrance to A81 footpath. Take footpath West parallel to Maryhill Road.",
-        "co_ordinates": "55.899418,-4.304363,48"
-    },
-    {
-        "name": "West of Scotland Science Park",
-        "description": "14. West of Scotland Science Park. Turn right towards Maryhill Road pavement and cycle path.",
-        "co_ordinates": "55.901402,-4.310006,40"
-    },
-    {
-        "name": "Maryhill Road",
-        "description": "15. Maryhill Road. Follow Maryhill Road (A81) North on pavement or cycleway.",
-        "co_ordinates": "55.901530,-4.309068,43"
-    },
-    {
-        "name": "Acre Roundabout",
-        "description": "16. Acre Roundabout. Follow Acre Road Northeast.",
-        "co_ordinates": "55.903554,-4.311331,41"
-    },
-    {
-        "name": "Acre Mini-Roundabout",
-        "description": "17. Acre Mini-Roundabout. Follow Acre Road Southeast.",
-        "co_ordinates": "55.904032,-4.310271,41"
-    },
-    {
-        "name": "Kelvin Walkway",
-        "description": "18. Kelvin Walkway. Rejoin the Walkway heading North.",
-        "co_ordinates": "55.904589,-4.300583,34"
-    },
-    {
-        "name": "Balmore Road and Balmuildy Bridge",
-        "description": "19. Opposite Balmore Road is the site of Balmuildy Fort, through which the Antonine Wall passed. At this point you can proceed along the South bank of the Kelvin, although the path will become muddy, or take Balmore Road South to Balmuildy Road.",
-        "co_ordinates": "55.918390,-4.274481,37"
-    },
-    {
-        "name": "Balmore Road and Balmuildy Road",
-        "description": "20. Head East along Balmuildy Road toward Wilderness Plantation.",
-        "co_ordinates": "55.915857,-4.271922,48"
-    },
-    {
-        "name": "Site of Balmuildy Fort",
-        "description": "20a. Site of Balmuildy Fort.",
-        "co_ordinates": "55.917310,-4.271073,43"
-    },
-    {
-        "name": "Archaeological find: Statuette of Mars Ultor",
-        "description": "20b. Archaeological find: Statuette of Mars Ultor.",
-        "co_ordinates": "55.917522,-4.270957,42"
-    },
-    {
-        "name": "Archaeological find: Head of a Nymph or Goddess",
-        "description": "20c. Archaeological find: Head of a Nymph or Goddess",
-        "co_ordinates": "55.917461,-4.270561,42"
-    },
-    {
-        "name": "Archaeological find: Statuette of Victory",
-        "description": "20d. Archaeological find: Statuette of Victory.",
-        "co_ordinates": "55.917559,-4.270364,41"
-    },
-    {
-        "name": "Archaeological find: Altar to Fortuna",
-        "description": "20e. Archaeological find: Altar to Fortuna",
-        "co_ordinates": "55.917678,-4.269605,40"
-    },
-    {
-        "name": "Antontine Wall and Balmuildy Road",
-        "description": "21. At this point the path of the Antonine Wall joins Balmuildy Road, remaining with it until Wilderness Plantation.",
-        "co_ordinates": "55.918576,-4.263241,55"
-    },
-    {
-        "name": "Wilderness Plantation",
-        "description": "22. Here Balmuildy Road turns South, but the path of the Antonine Wall continues East. Shortly, Balmuildy Road turns East to parallel the path of the Wall.",
-        "co_ordinates": "55.919792,-4.256208,62"
-    },
-    {
-        "name": "Balmuildy Road and Antonine Wall",
-        "description": "23. Here the Wall again converges with Balmuildy Road and its path can be seen quite clearly, running parallel with the road before entering Cawder Golf Club.",
-        "co_ordinates": "55.921295,-4.244254,75"
-    },
-    {
-        "name": "Site of Wilderness Plantation Fortlet",
-        "description": "23a. The fortlet is a short walk back northwest, just south of some barns.",
-        "co_ordinates": "55.921702,-4.245927,69"
-    },
-    {
-        "name": "Cawder Golf Club",
-        "description": "24. Cawder Golf Club. Turn South on Balmuildy Road.",
-        "co_ordinates": "55.922813,-4.237030,74"
-    },
-    {
-        "name": "Forth and Clyde Canal",
-        "description": "25. Forth and Clyde Canal. Turn West, taking the towpath on the North bank of the canal.",
-        "co_ordinates": "55.917345,-4.233350,51"
-    },
-    {
-        "name": "Possil Loch",
-        "description": "26. Possil Loch. This is an excellent site for birdwatching. Continue South on the canal towpath.",
-        "co_ordinates": "55.902734,-4.261402,49"
-    },
-    {
-        "name": "Lochburn Road",
-        "description": "27. Lochburn Road. Cross to the South bank of the canal and proceed North then Southwest.",
-        "co_ordinates": "55.892829,-4.286079,50"
-    },
-    {
-        "name": "Maryhill Locks",
-        "description": "28. Maryhill Locks. Continue West on the canal towpath.",
-        "co_ordinates": "55.892595,-4.296480,51"
-    },
-    {
-        "name": "Exit to Kelvin Walkway",
-        "description": "30. Exit to Kelvin Walkway. On the return, rejoin the Kelvin Walkway here and retrace to the start of the walk, from point 10.",
-        "co_ordinates": "55.892606,-4.300502,37"
-    },
+    }# ,
+    # {
+    #     "name": "North Woodside Flint Mill",
+    #     "description": "2. Remains of North Woodside Flint Mill.",
+    #     "co_ordinates": "55.877963,-4.282098,33"
+    # },
+    # {
+    #     "name": "Queen Margaret Bridge",
+    #     "description": "3. Queen Margaret Bridge.",
+    #     "co_ordinates": "55.879589,-4.285663,32"
+    # },
+    # {
+    #     "name": "Bridge to Botanic Gardens",
+    #     "description": "4. Bridge to Botanic Gardens.",
+    #     "co_ordinates": "55.880067,-4.287890,32"
+    # },
+    # {
+    #     "name": "Ha'penny Bridge House",
+    #     "description": "5. Ha'penny Bridge House. Take right fork following the River Kelvin on its South bank.",
+    #     "co_ordinates": "55.881264,-4.291263,29"
+    # },
+    # {
+    #     "name": "Return bridge from Botanic Gardens Arboretum",
+    #     "description": "6. Return bridge from Botanic Gardens Arboretum. Continue North on Kelvin Walkway.",
+    #     "co_ordinates": "55.884409,-4.289731,32"
+    # },
+    # {
+    #     "name": "Disused railway bridges",
+    #     "description": "7. Disused railway bridges.",
+    #     "co_ordinates": "55.885111,-4.291141,31"
+    # },
+    # {
+    #     "name": "Fork to Wyndford Estate",
+    #     "description": "8. Fork to Wyndford Estate. Follow Kelvin Walkway West.",
+    #     "co_ordinates": "55.885402,-4.292085,31"
+    # },
+    # {
+    #     "name": "Kelvindale Road",
+    #     "description": "9. Kelvindale Road. Cross and continue North on Kelvin Walkway.",
+    #     "co_ordinates": "55.887585,-4.298239,28"
+    # },
+    # {
+    #     "name": "Kelvin Aqueduct, turn-off for Forth and Clyde Canal",
+    #     "description": "10. Kelvin Aqueduct, turn-off for Forth and Clyde Canal. Continue North on Kelvin Walkway.",
+    #     "co_ordinates": "55.892040,-4.300411,36"
+    # },
+    # {
+    #     "name": "Maryhill Rail Bridge",
+    #     "description": "11. Maryhill Rail Bridge. Follow Kelvin Walkway Northwest.",
+    #     "co_ordinates": "55.896456,-4.303370,34"
+    # },
+    # {
+    #     "name": "Footbridge to Dawsholm Park",
+    #     "description": "12. Footbridge to Dawsholm Park. Turn right shortly afterward, heading uphill away from the river.",
+    #     "co_ordinates": "55.899041,-4.306004,39"
+    # },
+    # {
+    #     "name": "Entrance to A81 footpath",
+    #     "description": "13. Entrance to A81 footpath. Take footpath West parallel to Maryhill Road.",
+    #     "co_ordinates": "55.899418,-4.304363,48"
+    # },
+    # {
+    #     "name": "West of Scotland Science Park",
+    #     "description": "14. West of Scotland Science Park. Turn right towards Maryhill Road pavement and cycle path.",
+    #     "co_ordinates": "55.901402,-4.310006,40"
+    # },
+    # {
+    #     "name": "Maryhill Road",
+    #     "description": "15. Maryhill Road. Follow Maryhill Road (A81) North on pavement or cycleway.",
+    #     "co_ordinates": "55.901530,-4.309068,43"
+    # },
+    # {
+    #     "name": "Acre Roundabout",
+    #     "description": "16. Acre Roundabout. Follow Acre Road Northeast.",
+    #     "co_ordinates": "55.903554,-4.311331,41"
+    # },
+    # {
+    #     "name": "Acre Mini-Roundabout",
+    #     "description": "17. Acre Mini-Roundabout. Follow Acre Road Southeast.",
+    #     "co_ordinates": "55.904032,-4.310271,41"
+    # },
+    # {
+    #     "name": "Kelvin Walkway",
+    #     "description": "18. Kelvin Walkway. Rejoin the Walkway heading North.",
+    #     "co_ordinates": "55.904589,-4.300583,34"
+    # },
+    # {
+    #     "name": "Balmore Road and Balmuildy Bridge",
+    #     "description": "19. Opposite Balmore Road is the site of Balmuildy Fort, through which the Antonine Wall passed. At this point you can proceed along the South bank of the Kelvin, although the path will become muddy, or take Balmore Road South to Balmuildy Road.",
+    #     "co_ordinates": "55.918390,-4.274481,37"
+    # },
+    # {
+    #     "name": "Balmore Road and Balmuildy Road",
+    #     "description": "20. Head East along Balmuildy Road toward Wilderness Plantation.",
+    #     "co_ordinates": "55.915857,-4.271922,48"
+    # },
+    # {
+    #     "name": "Site of Balmuildy Fort",
+    #     "description": "20a. Site of Balmuildy Fort.",
+    #     "co_ordinates": "55.917310,-4.271073,43"
+    # },
+    # {
+    #     "name": "Archaeological find: Statuette of Mars Ultor",
+    #     "description": "20b. Archaeological find: Statuette of Mars Ultor.",
+    #     "co_ordinates": "55.917522,-4.270957,42"
+    # },
+    # {
+    #     "name": "Archaeological find: Head of a Nymph or Goddess",
+    #     "description": "20c. Archaeological find: Head of a Nymph or Goddess",
+    #     "co_ordinates": "55.917461,-4.270561,42"
+    # },
+    # {
+    #     "name": "Archaeological find: Statuette of Victory",
+    #     "description": "20d. Archaeological find: Statuette of Victory.",
+    #     "co_ordinates": "55.917559,-4.270364,41"
+    # },
+    # {
+    #     "name": "Archaeological find: Altar to Fortuna",
+    #     "description": "20e. Archaeological find: Altar to Fortuna",
+    #     "co_ordinates": "55.917678,-4.269605,40"
+    # },
+    # {
+    #     "name": "Antontine Wall and Balmuildy Road",
+    #     "description": "21. At this point the path of the Antonine Wall joins Balmuildy Road, remaining with it until Wilderness Plantation.",
+    #     "co_ordinates": "55.918576,-4.263241,55"
+    # },
+    # {
+    #     "name": "Wilderness Plantation",
+    #     "description": "22. Here Balmuildy Road turns South, but the path of the Antonine Wall continues East. Shortly, Balmuildy Road turns East to parallel the path of the Wall.",
+    #     "co_ordinates": "55.919792,-4.256208,62"
+    # },
+    # {
+    #     "name": "Balmuildy Road and Antonine Wall",
+    #     "description": "23. Here the Wall again converges with Balmuildy Road and its path can be seen quite clearly, running parallel with the road before entering Cawder Golf Club.",
+    #     "co_ordinates": "55.921295,-4.244254,75"
+    # },
+    # {
+    #     "name": "Site of Wilderness Plantation Fortlet",
+    #     "description": "23a. The fortlet is a short walk back northwest, just south of some barns.",
+    #     "co_ordinates": "55.921702,-4.245927,69"
+    # },
+    # {
+    #     "name": "Cawder Golf Club",
+    #     "description": "24. Cawder Golf Club. Turn South on Balmuildy Road.",
+    #     "co_ordinates": "55.922813,-4.237030,74"
+    # },
+    # {
+    #     "name": "Forth and Clyde Canal",
+    #     "description": "25. Forth and Clyde Canal. Turn West, taking the towpath on the North bank of the canal.",
+    #     "co_ordinates": "55.917345,-4.233350,51"
+    # },
+    # {
+    #     "name": "Possil Loch",
+    #     "description": "26. Possil Loch. This is an excellent site for birdwatching. Continue South on the canal towpath.",
+    #     "co_ordinates": "55.902734,-4.261402,49"
+    # },
+    # {
+    #     "name": "Lochburn Road",
+    #     "description": "27. Lochburn Road. Cross to the South bank of the canal and proceed North then Southwest.",
+    #     "co_ordinates": "55.892829,-4.286079,50"
+    # },
+    # {
+    #     "name": "Maryhill Locks",
+    #     "description": "28. Maryhill Locks. Continue West on the canal towpath.",
+    #     "co_ordinates": "55.892595,-4.296480,51"
+    # },
+    # {
+    #     "name": "Exit to Kelvin Walkway",
+    #     "description": "30. Exit to Kelvin Walkway. On the return, rejoin the Kelvin Walkway here and retrace to the start of the walk, from point 10.",
+    #     "co_ordinates": "55.892606,-4.300502,37"
+    # }
 ]
