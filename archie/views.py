dea92f98c7daf5b8898e61eebb1d4e48b9206218
Robert Dallas Gray <mail@robertdallasgray.com>
2012-03-28 21:46:49 +0100

    Added logic to create target records on target execution

--- archie/views.py
+++ archie/views.py
@@ -1,5 +1,5 @@
-from archie import app, models
-from flask import make_response, redirect, url_for, render_template
+from archie import app, models, db
+from flask import request, make_response, redirect, url_for, render_template
 
 # routes
 @app.route('/')
@@ -11,3 +11,10 @@ def pois():
     resp = make_response(render_template('pois.xml', pois=models.Poi.query.all()))
     resp.headers['Content-type'] = 'text/xml'
     return resp
+
+@app.route('/targets')
+def targets():
+    t = models.Target(**request.args.to_dict())
+    db.session.add(t)
+    db.session.commit()
+    return make_response(t.__repr__())
