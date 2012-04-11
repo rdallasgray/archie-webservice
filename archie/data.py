6212486b108c8b0ad92814301e8ec14c784f61f3
Robert Dallas Gray <mail@robertdallasgray.com>
2012-04-11 15:58:06 +0100

    Added thumbnail info to poi data.

--- archie/data.py
+++ archie/data.py
@@ -1,86 +1,101 @@
 data = [
     {
-        "name": "Kelvin Stevenson Memorial Parish Church",
+        "name": "Church",
         "description": "Kelvin Stevenson Memorial Church. Follow the steps down to Garriochmill Road.",
         "type": "waypoint",
         "waypoint_number" : "1",
-        "co_ordinates": "-4.278837,55.877773,33"
+        "co_ordinates": "-4.278837,55.877773,33",
+        "thumbnail": "ksm"
     },
     {
         "name": "Foundations",
         "description": "Foundations of North Woodside Colour Mill.",
         "type": "soi",
-        "co_ordinates": "-4.281980,55.877966,33"
+        "co_ordinates": "-4.281980,55.877966,33",
+        "thumbnail": "nwfm_foundations"
     },
     {
         "name": "Mill Wheels",
         "description": "Mill wheels from North Woodside Colour Mill.",
         "type": "soi",
-        "co_ordinates": "-4.282166,55.877895,32"
+        "co_ordinates": "-4.282166,55.877895,32",
+        "thumbnail": "nwfm_millwheel"
     },
     {
-        "name": "North Woodside Colour Mill",
+        "name": "Kiln",
+        "description": "The kiln of North Woodside Colour Mill.",
+        "type": "soi",
+        "co_ordinates": "-4.282273,55.877861,32",
+        "thumbnail": "nwfm_kiln"
+    },
+    {
+        "name": "Colour Mill",
         "description": "Remains of North Woodside Colour Mill.",
         "type": "waypoint",
         "waypoint_number" : "2",
-        "co_ordinates": "-4.282098,55.877963,33"
+        "co_ordinates": "-4.282098,55.877963,33",
+        "thumbnail": "nwfm_remains"
+
     },
     {
         "name": "Lade",
         "description": "The lade of North Woodside Colour Mill.",
         "type": "soi",
-        "co_ordinates": "-4.284397,55.878579,30"
+        "co_ordinates": "-4.284397,55.878579,30",
+        "thumbnail": "nwfm_lade"
     },
     {
         "name": "Dam",
         "description": "Dam marking the start of the lade of North Woodside Colour Mill.",
         "type": "view",
-        "co_ordinates": "-4.285153,55.878938,30"
+        "co_ordinates": "-4.285153,55.878938,30",
+        "thumbnail": "nwfm_dam"
     },
     {
-        "name": "Queen Margaret Bridge",
+        "name": "Bridge",
         "description": "Queen Margaret Bridge.",
         "type": "waypoint",
         "waypoint_number" : "3",
-        "co_ordinates": "-4.285663,55.879589,32"
+        "co_ordinates": "-4.285663,55.879589,32",
+        "thumbnail": "qmb"
     },
     {
-        "name": "Humpback Bridge",
+        "name": "Footbridge",
         "description": "Bridge to Botanic Gardens.",
         "type": "waypoint",
         "waypoint_number" : "4",
         "co_ordinates": "-4.287890,55.880067,32"
     },
     {
-        "name": "Ha'penny Bridge House",
+        "name": "Bridge House",
         "description": "Ha'penny Bridge House. Take right fork following the River Kelvin on its South bank.",
         "type": "waypoint",
         "waypoint_number" : "5",
         "co_ordinates": "-4.291263,55.881264,29"
     },
     {
-        "name": "Return bridge from Botanic Gardens Arboretum",
-        "description": "Return bridge from Botanic Gardens Arboretum. Continue North on Kelvin Walkway.",
+        "name": "Footbridge",
+        "description": "Garrioch Footbridge to Botanic Gardens Arboretum. Continue North on Kelvin Walkway.",
         "type": "waypoint",
         "waypoint_number" : "6",
         "co_ordinates": "-4.289731,55.884409,32"
     },
     {
-        "name": "Kirklee Bridge",
+        "name": "Bridge",
         "description": "Kirklee Bridge and site of Garrioch Flint Mill.",
         "type": "waypoint",
         "waypoint_number" : "7",
         "co_ordinates": "-4.291141,55.885111,31"
     },
     {
-        "name": "Fork to Wyndford Estate",
+        "name": "Fork",
         "description": "Fork to Wyndford Estate. Follow Kelvin Walkway West.",
         "type": "waypoint",
         "waypoint_number" : "8",
         "co_ordinates": "-4.292085,55.885402,31"
     },
     {
-        "name": "Kelvindale Road",
+        "name": "Road Crossing",
         "description": "Kelvindale Road. Cross and continue North on Kelvin Walkway.",
         "type": "waypoint",
         "waypoint_number" : "9",
@@ -93,14 +108,14 @@ data = [
         "co_ordinates": "-4.300241,55.891206,34"
     },
     {
-        "name": "Kelvin Aqueduct",
+        "name": "Aqueduct",
         "description": "Kelvin Aqueduct and turn-off for Forth and Clyde Canal. Continue North on Kelvin Walkway.",
         "type": "waypoint",
         "waypoint_number" : "10",
         "co_ordinates": "-4.300411,55.892040,36"
     },
     {
-        "name": "Maryhill Rail Bridge",
+        "name": "Bridge",
         "description": "Maryhill Rail Bridge. Follow Kelvin Walkway Northwest.",
         "type": "waypoint",
         "waypoint_number" : "11",
@@ -113,20 +128,20 @@ data = [
         "co_ordinates":  "-4.305696,55.897300,36"
     },
     {
-        "name": "Dawsholm Park",
+        "name": "Park",
         "description": "Footbridge to Dawsholm Park. Turn right shortly afterward, heading uphill away from the river.",
         "type": "waypoint",
         "waypoint_number" : "12",
         "co_ordinates": "-4.306004,55.899041,39"
     },
     {
-        "name": "Dawsholm Mill",
+        "name": "Mill",
         "description": "Site of Dawsholm Paper Mill.",
         "type": "soi",
         "co_ordinates": "-4.306715,55.899041,35"
     },
     {
-        "name": "A81 Footpath",
+        "name": "Footpath",
         "description": "Entrance to A81 footpath. Take footpath West parallel to Maryhill Road.",
         "type": "waypoint",
         "waypoint_number" : "13",
@@ -140,100 +155,100 @@ data = [
         "co_ordinates": "-4.310006,55.901402,40"
     },
     {
-        "name": "Maryhill Road",
+        "name": "Road",
         "description": "Maryhill Road. Follow Maryhill Road (A81) North on pavement or cycleway.",
         "type": "waypoint",
         "waypoint_number" : "15",
         "co_ordinates": "-4.309068,55.901530,43"
     },
     {
-        "name": "Acre Roundabout",
+        "name": "Roundabout",
         "description": "Acre Roundabout. Follow Acre Road Northeast.",
         "type": "waypoint",
         "waypoint_number" : "16",
         "co_ordinates": "-4.311331,55.903554,41"
     },
     {
-        "name": "Acre Mini-Roundabout",
+        "name": "Roundabout",
         "description": "Acre Mini-Roundabout. Follow Acre Road Southeast.",
         "type": "waypoint",
         "waypoint_number" : "17",
         "co_ordinates": "-4.310271,55.904032,41"
     },
     {
-        "name": "Kelvin Walkway",
+        "name": "Walkway",
         "description": "Kelvin Walkway. Rejoin the Walkway heading North.",
         "type": "waypoint",
         "waypoint_number" : "18",
         "co_ordinates": "-4.300583,55.904589,34"
     },
     {
-        "name": "Balmore Road and Balmuildy Bridge",
+        "name": "Bridge",
         "description": "Opposite Balmore Road is the site of Balmuildy Fort, through which the Antonine Wall passed. At this point you can proceed along the South bank of the Kelvin, although the path will become muddy, or take Balmore Road South to Balmuildy Road.",
         "type": "waypoint",
         "waypoint_number" : "19",
         "co_ordinates": "-4.274481,55.918390,37"
     },
     {
-        "name": "Balmore Road and Balmuildy Road",
+        "name": "Crossing",
         "description": "Head East along Balmuildy Road toward Wilderness Plantation.",
         "type": "waypoint",
         "waypoint_number" : "20",
         "co_ordinates": "-4.271922,55.915857,48"
     },
     {
-        "name": "Balmuildy Fort",
+        "name": "Fort",
         "description": "Site of Balmuildy Fort.",
         "type": "soi",
         "co_ordinates": "-4.271073,55.917310,43"
     },
     {
-        "name": "Statuette of Mars Ultor",
+        "name": "Find",
         "description": "Archaeological find: Statuette of Mars Ultor.",
         "type": "find",
         "co_ordinates": "-4.270957,55.917522,42"
     },
     {
-        "name": "Head of a Nymph",
+        "name": "Find",
         "description": "Archaeological find: Head of a Nymph or Goddess",
         "type": "find",
         "co_ordinates": "-4.270561,55.917461,42"
     },
     {
-        "name": "Statuette of Victory",
+        "name": "Find",
         "description": "Archaeological find: Statuette of Victory.",
         "type": "find",
         "co_ordinates": "-4.270364,55.917559,41"
     },
     {
-        "name": "Altar to Fortuna",
+        "name": "Find",
         "description": "Archaeological find: Altar to Fortuna",
         "type": "find",
         "co_ordinates": "-4.269605,55.917678,40"
     },
     {
-        "name": "Balmuildy Road",
+        "name": "Road",
         "description": "At this point the path of the Antonine Wall joins Balmuildy Road, remaining with it until Wilderness Plantation.",
         "type": "waypoint",
         "waypoint_number" : "21",
         "co_ordinates": "-4.263241,55.918576,55"
     },
     {
-        "name": "Wilderness Plantation",
+        "name": "Plantation",
         "description": "Here Balmuildy Road turns South, but the path of the Antonine Wall continues East. Shortly, Balmuildy Road turns East to parallel the path of the Wall.",
         "type": "waypoint",
         "waypoint_number" : "22",
         "co_ordinates": "-4.256208,55.919792,62"
     },
     {
-        "name": "Antonine Wall",
+        "name": "Wall",
         "description": "Here the Wall again converges with Balmuildy Road and its path can be seen quite clearly, running parallel with the road before entering Cawder Golf Club.",
         "type": "waypoint",
         "waypoint_number" : "23",
         "co_ordinates": "-4.244254,55.921295,75"
     },
     {
-        "name": "Site of Wilderness Plantation Fortlet",
+        "name": "Fortlet",
         "description": "The fortlet is a short walk back northwest, just south of some barns.",
         "type": "soi",
         "co_ordinates": "-4.245927,55.921702,69"
@@ -253,28 +268,28 @@ data = [
         "co_ordinates": "-4.233350,55.917345,51"
     },
     {
-        "name": "Possil Loch",
+        "name": "Loch",
         "description": "Possil Loch. This is an excellent site for birdwatching. Continue South on the canal towpath.",
         "type": "waypoint",
         "waypoint_number" : "26",
         "co_ordinates": "-4.261402,55.902734,49"
     },
     {
-        "name": "Lochburn Road",
+        "name": "Crossing",
         "description": "Lochburn Road. Cross to the South bank of the canal and proceed North then Southwest.",
         "type": "waypoint",
         "waypoint_number" : "27",
         "co_ordinates": "-4.286079,55.892829,50"
     },
     {
-        "name": "Maryhill Locks",
+        "name": "Locks",
         "description": "Maryhill Locks. Continue West on the canal towpath.",
         "type": "waypoint",
         "waypoint_number" : "28",
         "co_ordinates": "-4.296480,55.892595,51"
     },
     {
-        "name": "Exit to Kelvin Walkway",
+        "name": "Walkway",
         "description": "Exit to Kelvin Walkway. On the return, rejoin the Kelvin Walkway here and retrace to the start of the walk, from point 10.",
         "type": "waypoint",
         "waypoint_number" : "29",
