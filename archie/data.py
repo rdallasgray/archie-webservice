ba64903cf97a446bb181482644cd2ccfdc905390
Robert Dallas Gray <mail@robertdallasgray.com>
2012-05-18 17:34:24 +0100

    Adding media and POIs.

--- archie/data.py
+++ archie/data.py
@@ -212,9 +212,18 @@ data = [
     },
     {
         "name": "Piers",
-        "description": "Bridge piers remaining from the site of Kelvindale paper mills.",
+        "description": "Piers remaining from a viaduct which carried the Glasgow Central Railway Dawsholm branch, 1896-1960s.",
         "type": "view",
         "co_ordinates": "-4.30121,55.89176,34",
+        "thumbnail": "kd_bridge",
+        "attachments": [
+            {
+                "name": "The bridge in 1965.",
+                "type": "image",
+                "url": "kd_bridge.jpg",
+                "thumbnail": "kd_bridge"
+            },
+        ]
     },
     {
         "name": "Canal",
@@ -335,9 +344,30 @@ data = [
     },
     {
         "name": "Mill",
-        "description": "Site of Dawsholm Paper Mill.",
+        "description": "Site of Dawsholm Paper Mill. The mill operated for at least 200 years, finally closing in 1970.",
         "type": "soi",
-        "co_ordinates": "-4.30671,55.89905,35"
+        "co_ordinates": "-4.30671,55.89905,35",
+        "thumbnail": "dhpm_01",
+        "attachments": [
+            {
+                "name": "Dawsholm Paper Mills, 1965.",
+                "type": "image",
+                "url": "dhpm_01.jpg",
+                "thumbnail": "dhpm_01"
+            },
+            {
+                "name": "Dawsholm Paper Mills, 1965.",
+                "type": "image",
+                "url": "dhpm_02.jpg",
+                "thumbnail": "dhpm_02"
+            },
+            {
+                "name": "Dawsholm Paper Mills, 1965.",
+                "type": "image",
+                "url": "dhpm_03.jpg",
+                "thumbnail": "dhpm_03"
+            },
+        ]
     },
     {
         "name": "Footpath",
@@ -369,81 +399,171 @@ data = [
     },
     {
         "name": "Roundabt",
-        "description": "Acre Mini-Roundabout. Follow Acre Road Southeast.",
+        "description": "Acre Mini-Roundabout. Follow Acre Road Southeast then East.",
         "type": "waypoint",
         "waypoint_number" : "17",
         "co_ordinates": "-4.31027,55.90403,41"
     },
     {
+        "name": "Observtry",
+        "description": "Glasgow University Acre Road Observatory.",
+        "type": "soi",
+        "co_ordinates": "-4.30790,55.90253,41"
+    },
+    {
+        "name": "House",
+        "description": "Acre House, c. 1880, around which the estate of Acre grew. Used by University of Glasgow for some time and sold in 2006, since when it has lain derelict.",
+        "type": "soi",
+        "co_ordinates": "-4.30674,55.90378,50"
+    },
+    {
+        "name": "Turning",
+        "description": "Turning to Kelvin Walkway. Head north with open field on the right to reach the river.",
+        "type": "waypoint",
+        "waypoint_number" : "18",
+        "co_ordinates": "-4.30085,55.90394,38"
+    },
+    {
         "name": "Walkway",
         "description": "Kelvin Walkway. Rejoin the Walkway heading North, passing the Riding School on the right and Killermont Golf Course on the far bank. Follow the river for a long, winding stretch until Balmuildy Bridge.",
         "type": "waypoint",
-        "waypoint_number" : "18",
-        "co_ordinates": "-4.30058,55.90459,34"
+        "waypoint_number" : "19",
+        "co_ordinates": "-4.30006,55.90534,34"
+    },
+    {
+        "name": "House",
+        "description": "Killermont House (c. 1800). Now the club house of Killermont Golf Club (the world's ninth oldest).",
+        "type": "view",
+        "co_ordinates": "-4.30210,55.90840,45",
+        "thumbnail": "kmh",
+        "attachments": [
+            {
+                "name": "Killermont House. Photograph by Thomas Annan, 1855.",
+                "type": "image",
+                "url": "kmh.jpg",
+                "thumbnail": "kmh"
+            },
+        ]
+    },
+    {
+        "name": "Railway",
+        "description": "Part of the path of the dismantled Kelvin Valley Railway, which ran between Kilsyth and Maryhill until 1966.",
+        "type": "soi",
+        "co_ordinates": "-4.28724,55.91097,41",
+        "thumbnail": "kmh",
+        "attachments": [
+            {
+                "name": "Killermont House. Photograph by Thomas Annan, 1855.",
+                "type": "image",
+                "url": "kmh.jpg",
+                "thumbnail": "kmh"
+            },
+        ]
     },
     {
         "name": "Bridge",
         "description": "Opposite Balmore Road is the site of Balmuildy Fort, through which the Antonine Wall passed. At this point you can proceed along the South bank of the Kelvin, although the path will become muddy, or take Balmore Road South to Balmuildy Road.",
         "type": "waypoint",
-        "waypoint_number" : "19",
+        "waypoint_number" : "20",
         "co_ordinates": "-4.27448,55.91839,37"
     },
     {
         "name": "Crossing",
         "description": "Head East along Balmuildy Road toward Wilderness Plantation.",
         "type": "waypoint",
-        "waypoint_number" : "20",
+        "waypoint_number" : "21",
         "co_ordinates": "-4.27192,55.91586,48"
     },
     {
         "name": "Fort",
-        "description": "Site of Balmuildy Fort.",
+        "description": "Site of Balmuildy Fort. The fort is no longer visible, but the path of the Antonine Wall can sometimes be discerned, depending on conditions.",
         "type": "soi",
-        "co_ordinates": "-4.27107,55.91731,43"
+        "co_ordinates": "-4.27107,55.91731,43",
+        "thumbnail": "bm_fort",
+        "attachments": [
+            {
+                "name": "Plan of Balmuildy Fort, showing the path of the Antonine Wall and military way.",
+                "type": "image",
+                "url": "bm_fort.jpg",
+                "thumbnail": "bm_fort"
+            },
+            {
+                "name": "A wider view, showing the temporary camp northeast of the fort.",
+                "type": "image",
+                "url": "bm_fort_wide.jpg",
+                "thumbnail": "bm_fort_wide"
+            },
+            {
+                "name": "Aerial view. The path of the Wall can be seen between the road and river, crossing the hedgerow",
+                "type": "image",
+                "url": "bm_fort_aerial.jpg",
+                "thumbnail": "bm_fort_aerial"
+            },
+        ]
     },
     {
         "name": "Find",
         "description": "Archaeological find: Statuette of Mars Ultor.",
         "type": "find",
-        "co_ordinates": "-4.27096,55.91752,42"
+        "co_ordinates": "-4.27096,55.91752,42",
+        "thumbnail": "mars_ultor",
+        "attachments": [
+            {
+                "name": "Statuette of Mars Ultor, c. 142-180 AD.",
+                "type": "image",
+                "url": "mars_ultor.jpg",
+                "thumbnail": "mars_ultor"
+            },
+        ]
     },
     {
         "name": "Find",
         "description": "Archaeological find: Head of a Nymph or Goddess",
         "type": "find",
-        "co_ordinates": "-4.27056,55.91746,42"
-    },
-    {
-        "name": "Find",
-        "description": "Archaeological find: Statuette of Victory.",
-        "type": "find",
-        "co_ordinates": "-4.27036,55.91756,41"
+        "co_ordinates": "-4.27056,55.91746,42",
+        "thumbnail": "nymph",
+        "attachments": [
+            {
+                "name": "Head of a Nymph or Goddess, c. 142-180 AD.",
+                "type": "image",
+                "url": "nymph.jpg",
+                "thumbnail": "nymph"
+            },
+        ]
     },
     {
         "name": "Find",
         "description": "Archaeological find: Altar to Fortuna",
         "type": "find",
-        "co_ordinates": "-4.26960,55.91768,40"
+        "co_ordinates": "-4.26960,55.91768,40",
+        "attachments": [
+            {
+                "name": "Altar to Fortuna, Mid 2nd C. AD.",
+                "type": "image",
+                "url": "altar.jpg",
+                "thumbnail": "altar"
+            },
+        ]
     },
     {
         "name": "Road",
         "description": "At this point the path of the Antonine Wall joins Balmuildy Road, remaining with it until Wilderness Plantation.",
         "type": "waypoint",
-        "waypoint_number" : "21",
+        "waypoint_number" : "22",
         "co_ordinates": "-4.26324,55.91858,55"
     },
     {
         "name": "Plantation",
         "description": "Here Balmuildy Road turns South, but the path of the Antonine Wall continues East. Shortly, Balmuildy Road turns East to parallel the path of the Wall.",
         "type": "waypoint",
-        "waypoint_number" : "22",
+        "waypoint_number" : "23",
         "co_ordinates": "-4.25621,55.91979,62"
     },
     {
         "name": "Wall",
         "description": "Here the Wall again converges with Balmuildy Road and its path can be seen quite clearly, running parallel with the road before entering Cawder Golf Club.",
         "type": "waypoint",
-        "waypoint_number" : "23",
+        "waypoint_number" : "24",
         "co_ordinates": "-4.24425,55.92129,75"
     },
     {
@@ -456,42 +576,42 @@ data = [
         "name": "Golf Club",
         "description": "Cawder Golf Club. Turn South on Balmuildy Road.",
         "type": "waypoint",
-        "waypoint_number" : "24",
+        "waypoint_number" : "25",
         "co_ordinates": "-4.23703,55.92281,74"
     },
     {
         "name": "Canal",
         "description": "Forth and Clyde Canal. Turn West, taking the towpath on the North bank of the canal.",
         "type": "waypoint",
-        "waypoint_number" : "25",
+        "waypoint_number" : "26",
         "co_ordinates": "-4.23335,55.91734,51"
     },
     {
         "name": "Loch",
         "description": "Possil Loch. This is an excellent site for birdwatching. Continue South on the canal towpath.",
         "type": "waypoint",
-        "waypoint_number" : "26",
+        "waypoint_number" : "27",
         "co_ordinates": "-4.26140,55.90273,49"
     },
     {
         "name": "Crossing",
         "description": "Lochburn Road. Cross to the South bank of the canal and proceed North then Southwest.",
         "type": "waypoint",
-        "waypoint_number" : "27",
+        "waypoint_number" : "28",
         "co_ordinates": "-4.28608,55.89283,50"
     },
     {
         "name": "Locks",
         "description": "Maryhill Locks. Continue West on the canal towpath.",
         "type": "waypoint",
-        "waypoint_number" : "28",
+        "waypoint_number" : "29",
         "co_ordinates": "-4.29648,55.89259,51"
     },
     {
         "name": "Walkway",
         "description": "Exit to Kelvin Walkway. On the return, rejoin the Kelvin Walkway here and retrace to the start of the walk, from point 10.",
         "type": "waypoint",
-        "waypoint_number" : "29",
+        "waypoint_number" : "30",
         "co_ordinates": "-4.30050,55.89261,37"
     }
 ]
